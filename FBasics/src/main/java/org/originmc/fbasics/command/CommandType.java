0200fcd716d7f100520589679c4b20dfb639d65c
LocutusV0nB0rg <leon@familie-breidt.de>
2021-05-13 00:06:58 +0200

    Removed version command and changed no permission message

--- FBasics/src/main/java/org/originmc/fbasics/command/CommandType.java
+++ FBasics/src/main/java/org/originmc/fbasics/command/CommandType.java
@@ -14,8 +14,6 @@ public enum CommandType {
 
     SAFEPROMOTE(CmdSafepromote.class, new String[]{"safepromote", "sp", "s"}),
 
-    VERSION(CmdVersion.class, new String[]{"version", "v"}),
-
     WILDERNESS(CmdWilderness.class, new String[]{"wilderness", "wild", "rtp"});
 
     private static final HashMap<String, CommandType> BY_ALIAS = new HashMap<>();
